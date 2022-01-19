d8f19888b257e15fe9dbaf9ad1fd469aa3219dd5
Mykyta Medvediev <arlekin8881@gmail.com>
2022-01-19 13:28:28 -0800

    Added functionality for 'clear' and 'delete' buttons. Added internal representation of values on the screen. Digit and sign buttons now place their respective values on the lower screen.

--- script.js
+++ script.js
@@ -31,3 +31,55 @@ function operate(operator, x, y) {
     }
 }
 
+let display_values = "";
+
+const lowerScreen = document.getElementById('lower-screen');
+const upperScreen = document.getElementById('upper-screen');
+
+const digitBtns = document.querySelectorAll(".digit");
+digitBtns.forEach(digitBtn => digitBtn.addEventListener('click', function(e) {
+    lowerScreen.textContent += e.target.textContent;
+    display_values += e.target.id;
+    // console.log(display_values); 
+}));
+
+const  signBtns = document.querySelectorAll('.sign');
+signBtns.forEach(signBtn => signBtn.addEventListener('click', function(e) {
+    lowerScreen.textContent += " " + e.target.textContent + " ";
+    display_values += e.target.id;
+}))
+
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', () => {
+    upperScreen.textContent = "";
+    lowerScreen.textContent = "";
+    display_values = "";
+});
+
+const deleteBtn = document.getElementById('delete');
+deleteBtn.addEventListener('click', () => {
+    let displayArr = lowerScreen.textContent.split("");
+    let internalArr = display_values.split("");
+
+    if (displayArr[displayArr.length - 1] == " ") {
+        for (i = 0; i < 3; i++){
+            displayArr.pop();
+        }
+    } else {
+        displayArr.pop()
+    }
+    internalArr.pop()
+
+    lowerScreen.textContent = displayArr.join("");
+    display_values = internalArr.join("");
+})
+
+// 1. Check if display already has digits or not
+// 2. If not, turn sign buttons off
+
+// 3. Check if display already has a sign
+// 4. If yes, but no digits after, turn sign buttons off
+// 5. If yes, and there are digits after, calculate and place results into upper screen.
+
+// 6. Make buttons hoverable.
+// 7. Increase padding-right on the lower screen.
